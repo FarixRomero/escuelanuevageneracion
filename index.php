4a70d1fd06567358fea0cb3c3a6eb0710fcb06c6
Farix Romero <farixromero123@gmail.com>
2020-02-25 13:45:20 -0500

    avance nueva generacion v1

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="fontawesome\css\all.css" rel="stylesheet">
+    <link href="main.css" rel="stylesheet">
+
+    <!--load all styles -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class='showcase'>
+        <div class='content'>
+            <img src="logo.jpg" class="logo" alt="escuela">
+            <div class="tittle">
+                Bienvenido a la escuela de baile Nueva Generacion
+            </div>
+            <div class='text'>
+                <p>ipsum dolor sit amet, consectetur adipisicing elit. Sunt, sit!</p>
+
+            </div>
+        </div>
+    </header>
+    <!--services-->
+    <section class="services">
+        <div class="conteiner grid-3 center ">
+            <div>
+                <img src="https://img.icons8.com/ios/100/000000/ballroom-dance.png" class="bailedance">
+                <h3>Baile</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, architecto. Consequuntur ratione animi dolor neque porro labore pariatur sunt at!</p>
+            </div>
+            <div>
+                <i class="fas fa-palette fa-3x"></i>
+                <h3>Arte</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, architecto. Consequuntur ratione animi dolor neque porro labore pariatur sunt at!</p>
+            </div>
+            <div>
+                <i class="fas fa-theater-masks fa-3x"></i>
+                <h3>Teatro</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, architecto. Consequuntur ratione animi dolor neque porro labore pariatur sunt at!</p>
+            </div>
+            <div>
+                <i class="fas fa-music fa-3x"></i>
+                <h3>Canto</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, architecto. Consequuntur ratione animi dolor neque porro labore pariatur sunt at!</p>
+            </div>
+        </div>
+    </section>
+    <!---About-->
+    <section class="about bg-light">
+        <div class="conteiner">
+            <div class="grid-2">
+                <div class="center">
+                    <img class='tiago' src="bailenueva.jpg" alt="escuela">
+                </div>
+                <div>
+                    <h3>¿Quienes somos nosotros?</h3>
+                    <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi voluptatibus asperiores ipsum officiis? Doloremque rem accusamus minima!</P>
+
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+    <footer class="center bg-dark">
+        <p>Escuela Nueva Generacion en colaboracion con Farix Romero 2020</p>
+
+
+    </footer>
+
+
+
+
+
+
+</body>
+
+</html>
